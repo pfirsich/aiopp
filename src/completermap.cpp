f39c22612e45a5642438d3f2b98946844f4ce5ab
Joel Schumacher <joelschum@gmail.com>
2023-05-11 23:41:53 +0200

    Use double hashing in CompleterMap
    More information including a benchmark and results can be found in the
    'completermap' branch. Specifically this commit:
    36c365a5da298225920429c5cff634d468da8fbf

--- src/completermap.cpp
+++ src/completermap.cpp
@@ -13,11 +13,25 @@ CompleterMap::CompleterMap(size_t numEntries)
 {
 }
 
+namespace {
+    // SplitMix64: https://nullprogram.com/blog/2018/07/31/
+    static uint64_t hash(uint64_t x)
+    {
+        x ^= x >> 30;
+        x *= 0xbf58476d1ce4e5b9U;
+        x ^= x >> 27;
+        x *= 0x94d049bb133111ebU;
+        x ^= x >> 31;
+        return x;
+    }
+}
+
 void CompleterMap::insert(uint64_t key, void* value)
 {
     assert(size() < capacity());
+    const auto h = (hash(key) % (entries_.size() - 1)) + 1;
     for (size_t i = 0; i < entries_.size(); ++i) {
-        auto& entry = entries_[(key + i) % entries_.size()];
+        auto& entry = entries_[(key + i * h) % entries_.size()];
         if (entry.value == Empty || entry.value == Tombstone) {
             entry.key = key;
             entry.value = value;
@@ -66,8 +80,9 @@ size_t CompleterMap::getNextSize(size_t num)
 
 std::optional<size_t> CompleterMap::lookup(uint64_t key) const
 {
+    const auto h = (hash(key) % (entries_.size() - 1)) + 1;
     for (size_t i = 0; i < entries_.size(); ++i) {
-        const auto idx = (key + i) % entries_.size();
+        const auto idx = (key + i * h) % entries_.size();
         auto& entry = entries_[idx];
         if (entry.value == Empty) {
             return std::nullopt;
